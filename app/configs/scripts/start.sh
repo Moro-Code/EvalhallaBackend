5d36298e9c08456e656703cf75b65d0ccafc9c1e
Omar Nasr <omar@omarnasr.ca>
2019-11-30 17:02:17 -0500

    rabbitmq management and flask working

--- app/configs/scripts/start.sh
+++ app/configs/scripts/start.sh
@@ -6,9 +6,36 @@ echo "Starting application in ${APP_ENV} environment"
 if [ "$APP_ENV" == "development" ] 
 then
    echo "Enabling management console for development environment"
-   rabbitmq-plugins enable rabbitmq_management 
+   rabbitmq-plugins enable rabbitmq_management
+   echo "Starting rabbitmq server process "
+   service rabbitmq-server start
+   echo "Adding admin user"
+   rabbitmqctl add_user dev_inst dev_inst
+   rabbitmqctl set_user_tags dev_inst administrator
+   rabbitmqctl set_permissions -p / dev_inst ".*" ".*" ".*"
+else
+   echo "Starting rabbitmq server process "
+   service rabbitmq-server start
 fi
 
-echo "Starting rabbitmq server process "
-service rabbitmq-server start
+
+NUM_CORES=$( getconf _NPROCESSORS_ONLN )
+echo "$NUM_CORES cpu cores detected on system"
+
+echo "Starting nginx service"
+service nginx start
+
+
+source venv/bin/activate
+if [ "$APP_ENV" == "development" ]
+then 
+   echo "Starting application in development mode"
+   flask run -p 8000
+else
+   echo "Starting application with $NUM_CORES workers"
+   gunicorn -w $NUM_CORES app:app
+fi
+
+
+
 
