6875b7d7ccce296a2343cdbc2433a49f9e2ddbf0
Moro-Code <omardev9898@gmail.com>
2019-12-02 13:07:15 -0500

    api boilerplate

--- app/src/database/models/SurveyResponseModel.py
+++ app/src/database/models/SurveyResponseModel.py
@@ -1,14 +1,16 @@
 from .BaseModel import Base
-from sqlalchemy import Integer, ForeignKey, Column, DateTime
+from sqlalchemy import Integer, ForeignKey, Column, DateTime, Boolean
 from .utils import utcnow
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.sql import expression
 
 
 class SurveyResponseModel(Base):
     __tablename__ = "survey_responses"
     id = Column("id", Integer, primary_key=True)
     response = Column("response", JSONB, nullable = False)
-    createdOn = Column("createdOn", DateTime, server_default=utcnow())
+    createdOn = Column("createdOn", DateTime, server_default=utcnow(), nullable = False)
+    boolean = Column("processed", Boolean, server_default=expression.true() , nullable = False)
     evaleseId = Column("evalese_id_fk", Integer, ForeignKey(
         "evalese.id", 
         ondelete="CASCADE",
--- app/src/utils/errors/messages.py
+++ app/src/utils/errors/messages.py
@@ -2,9 +2,18 @@
 
 CONFIG_VAR_DOES_NOT_EXIST = (
       "{config_var} is a required configuration variable " +
-      "both the config value and its environment variable EVALHALLA_{config_var} is None"
+      "both the config value and its environment variable EVALHALLA_{config_var} is None "
 )
 CONFIG_IS_WRONG_TYPE = (
     "The config variable {config_var} is of incorrect type, expected {config_var_type} " +
-    "instead recieved {config_incorrect_type}"
+    "instead recieved {config_incorrect_type} "
+)
+
+ARGUMENT_MUST_BE_PROVIDED = (
+    "The argument '{argument}' must be provided "
+)
+
+
+ARGUMENT_IS_INCORRECT_TYPE = (
+    "The argument '{argument}' is of incorrect type {incorrect_type}. Argument should be of type {expected_type}"
 )
