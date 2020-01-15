d58183d7eebace0705bd34a0e1720f534e46717f
Moro-Code <omardev9898@gmail.com>
2020-01-14 23:38:25 -0500

    column name migration

--- app/src/database/models/SurveyResponseModel.py
+++ app/src/database/models/SurveyResponseModel.py
@@ -9,7 +9,7 @@ class SurveyResponseModel(Base):
     __tablename__ = "survey_responses"
     id = Column("id", Integer, primary_key=True)
     response = Column("response", JSONB, nullable = False)
-    createdOn = Column("createdOn", DateTime, server_default=utcnow(), nullable = False)
+    createdOn = Column("created_on", DateTime, server_default=utcnow(), nullable = False)
     uuid = Column("uuid", UUID, unique = True, server_default=text("uuid_generate_v4()"))
     processed = Column("processed", Boolean, server_default="false" , nullable = False)
     evaleseId = Column("evalese_id_fk", Integer, ForeignKey(
